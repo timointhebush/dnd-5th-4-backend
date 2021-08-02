5241db14941ef318e5d10872d32d358e312dd64d
timointhebush <junzzang97@naver.com>
2021-08-02 16:08:26 +0900

    Add: Dress Repository, Service 추가

--- src/main/java/dnd/team4backend/repository/DressRepository.java
+++ src/main/java/dnd/team4backend/repository/DressRepository.java
@@ -1,2 +1,27 @@
-package dnd.team4backend.repository;public class DressRepository {
+package dnd.team4backend.repository;
+
+import dnd.team4backend.domain.Dress;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class DressRepository {
+    private final EntityManager em;
+
+    public DressRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Dress dress) {
+        if (dress.getId() == null) {
+            em.persist(dress);
+        } else {
+            em.merge(dress);
+        }
+    }
+
+    public Dress findOne(Long id) {
+        return em.find(Dress.class, id);
+    }
 }
