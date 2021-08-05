f46fb42f8d1a1616bdd12feb2cf6dddbf16e4bdb
hhlee <whanhee97@naver.com>
2021-08-05 18:13:28 +0900

    Add: 유저 드레스 리스트 api 추가

--- src/main/java/dnd/team4backend/controller/UserController.java
+++ src/main/java/dnd/team4backend/controller/UserController.java
@@ -1,12 +1,17 @@
 package dnd.team4backend.controller;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import dnd.team4backend.domain.Dress;
 import dnd.team4backend.domain.User;
 import dnd.team4backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -50,4 +55,47 @@ public class UserController {
         }
 
     }
+
+    @GetMapping(value = "user/dresses")
+    public String userDressList(@RequestBody UserForm userForm) {
+        User user = userService.findOne(userForm.getUserId());
+        if (user == null) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("status", 400);
+            obj.addProperty("msg", "해당하는 id의 회원이 존재하지 않습니다.");
+
+            return obj.toString();
+        }
+
+        List<Dress> dressList = user.getDressList();
+        try {
+            JsonObject obj = new JsonObject();
+
+            JsonArray jsonArray = new JsonArray();
+            for (Dress dress : dressList) {
+                JsonObject dressObject = new JsonObject();
+                dressObject.addProperty("id", dress.getId());
+                dressObject.addProperty("userId", dress.getUser().getId());
+                dressObject.addProperty("name", dress.getDressName());
+                dressObject.addProperty("type", dress.getDressType().toString());
+                jsonArray.add(dressObject);
+            }
+
+            obj.add("dresses", jsonArray);
+            obj.addProperty("status", 200);
+            obj.addProperty("msg", "유저의 드레스리스트가 성공적으로 조회 되었습니다.");
+            return obj.toString();
+
+        } catch (IllegalStateException e) {
+            JsonObject obj = new JsonObject();
+
+            obj.addProperty("status", 400);
+            obj.addProperty("msg", e.getMessage());
+
+            return obj.toString();
+        }
+
+
+    }
+
 }
