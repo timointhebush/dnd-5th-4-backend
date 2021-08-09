88da2111dad3b3b348923eed6cdeff7557f0f9aa
hhlee <whanhee97@naver.com>
2021-08-09 18:27:35 +0900

    Add: 옷 삭제 API 구현

--- src/main/java/dnd/team4backend/repository/DressRepository.java
+++ src/main/java/dnd/team4backend/repository/DressRepository.java
@@ -1,29 +1,8 @@
 package dnd.team4backend.repository;
 
 import dnd.team4backend.domain.Dress;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-@Repository
-public class DressRepository {
-    @PersistenceContext
-    private final EntityManager em;
-
-    public DressRepository(EntityManager em) {
-        this.em = em;
-    }
-
-    public void save(Dress dress) {
-        if (dress.getId() == null) {
-            em.persist(dress);
-        } else {
-            em.merge(dress);
-        }
-    }
-
-    public Dress findOne(Long id) {
-        return em.find(Dress.class, id);
-    }
+public interface DressRepository extends JpaRepository<Dress, Long> {
+    
 }
