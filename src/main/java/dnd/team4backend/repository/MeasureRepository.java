149bba06b12bd52535326d6409551d12767ba8b4
timointhebush <junzzang97@naver.com>
2021-08-09 17:07:56 +0900

    Add: MeasureRepository findByDateBetween메소드 추가

--- src/main/java/dnd/team4backend/repository/MeasureRepository.java
+++ src/main/java/dnd/team4backend/repository/MeasureRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface MeasureRepository extends JpaRepository<Measure, Long> {
     /**
      * 특정 유저의 평가 중, 해당 날씨 데이터와 유사한
@@ -21,4 +24,6 @@ public interface MeasureRepository extends JpaRepository<Measure, Long> {
      */
     @Query("select m from Measure m where m.id < ?1 and m.user <> ?2 and (m.temperatureHigh between ?3 and ?4 or m.temperatureLow between ?5 and ?6 or m.humidity between ?7 and ?8) order by m.id desc")
     Page<Measure> findByWeatherOfOthers(Long measureId, User user, Float tempHigh1, Float tempHigh2, Float tempLow1, Float tempLow2, Float humid1, Float humid2, Pageable pageable);
+
+    List<Measure> findByDateBetween(LocalDateTime fromLocalDateTime, LocalDateTime toLocalDateTime);
 }
